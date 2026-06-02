eb588d981784fbfa89486f5ce78d466387c63f90
ramadhani01 <45001162+ramadhani01@users.noreply.github.com>
2026-06-02 17:45:39 +0700

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -2,16 +2,16 @@ const nodemailer = require('nodemailer');
 
 // ========== GANTI DENGAN 10 EMAIL DAN APP PASSWORD ASLI ANDA ==========
 const SENDER_ACCOUNTS = [
-        { email: "rizkyramadhani060601@mail.com", password: "eryekpwtwwahcujm" },
-        { email: "fianajah080801@mail.com", password: "qsmdlfdafswnuewy" },
-        { email: "akuunn0033@mail.com", password: "rgvqktuhpectmqnf" },
-        { email: "akuunn0044@mail.com", password: "bkplhrelgnjlgenb" },
-        { email: "akuunn0066@mail.com", password: "ofyxmsttjbaizaoy" },
-        { email: "akuunn100@mail.com", password: "vaawmqkvleuptlst" },
-        { email: "lmgacor0066@mail.com", password: "prjmvsuvkvxqoskx" },
-        { email: "akuunn1001@mail.com", password: "vrngwowpcvxrzgxz" },
-        { email: "akuunn2001@mail.com", password: "qwvwppmkqslkbcpi" },
-        { email: "rimada060606@mail.com", password: "jhzicmygeuobgxtd" }
+        { email: "rizkyramadhani060601@gmail.com", password: "eryekpwtwwahcujm" },
+        { email: "fianajah080801@gmail.com", password: "qsmdlfdafswnuewy" },
+        { email: "akuunn0033@gmail.com", password: "rgvqktuhpectmqnf" },
+        { email: "akuunn0044@gmail.com", password: "bkplhrelgnjlgenb" },
+        { email: "akuunn0066@gmail.com", password: "ofyxmsttjbaizaoy" },
+        { email: "akuunn100@gmail.com", password: "vaawmqkvleuptlst" },
+        { email: "lmgacor0066@gmail.com", password: "prjmvsuvkvxqoskx" },
+        { email: "akuunn1001@gmail.com", password: "vrngwowpcvxrzgxz" },
+        { email: "akuunn2001@gmail.com", password: "qwvwppmkqslkbcpi" },
+        { email: "rimada060606@gmail.com", password: "jhzicmygeuobgxtd" }
 ];
 
 const getTransporter = (email, password) => {
